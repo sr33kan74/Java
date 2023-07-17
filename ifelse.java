6a3aeaf67191f15c3b1619de6d5c0765d5a37f29
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-07-17 22:17:31 +0530

    if else clause in java

---
+++ ifelse.java
@@ -0,0 +1,13 @@
+//if else clause in java
+public class ifelse {
+    public static void main(String[] args) {
+        boolean learning = true;
+
+        if (learning) {
+            System.out.println("Java programmer");
+        }
+        else {
+            System.out.println("What are you doing here?");
+        }
+    }
+}
