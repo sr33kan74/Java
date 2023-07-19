37624a950b60eae34cc08f523adce3c220229a70
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-07-19 20:44:57 +0530

    Java program using loop

---
+++ loop.java
@@ -0,0 +1,10 @@
+// Java program using loop
+public class loop {
+    public static void main (String args[]) {
+        int c;
+
+        for (c=1; c<=10; c++) {
+            System.out.println(c);
+        }
+    }
+}
