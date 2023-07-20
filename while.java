d7b3d924cf3d0b431517d1b59d536518a4288332
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-07-20 23:32:36 +0530

    java program using while loop

---
+++ while.java
@@ -0,0 +1,16 @@
+// while loop in java
+import java.util.Scanner;
+public class while {
+    public static void main(String args[]) {
+        int n;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input an integer");
+
+        while ((n = input.nextInt()) !=0) {
+            System.out.println("You entered " +n);
+            System.out.println("Input an integer");
+        }
+        System.out.println("Out of loop");
+    }
+}
