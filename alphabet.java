028b0076be4763baac36d467b5277400559545c8
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-07-22 23:32:12 +0530

    Java program to print all alphabets

---
+++ alphabet.java
@@ -0,0 +1,8 @@
+// program to print all alphabets using loop in java
+public class alphabet {
+    public static void main (String args[]) {
+        char ch;
+        for(ch = 'a'; ch <='z'; ch++)
+            System.out.println(ch);
+    }
+}
