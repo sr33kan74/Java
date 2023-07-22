c67f75b8f1b6a6f404bcfdf1be7f395003fb4d77
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-07-22 23:36:58 +0530

    java forloop

---
+++ forloop2.java
@@ -0,0 +1,10 @@
+// java program using enhance loop
+public class forloop2 {
+    public static void main (String args[]) {
+        int primes[] = {2,3,5,7,11,13,17,19,23,29};
+
+        for (int t: primes) {
+            System.out.println(t);
+        }
+    }
+}
