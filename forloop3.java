eaf1cb175dc34635df82a2b1ba9740633a4cc0bc
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-07-22 23:42:14 +0530

    java for loop 3

---
+++ forloop3.java
@@ -0,0 +1,9 @@
+// java program using enhanced for loop
+public class forloop3 {
+    public static void main(String args[]) {
+        String languages[] = {"C", "C++", "java", "Python", "Ruby"};
+        for (String sample: languages) {
+            System.out.println(sample);
+        }
+    }
+}
