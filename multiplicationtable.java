f6c14d48253fd2bd147f6c7f4da532a6164b1905
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-07-23 23:21:02 +0530

    print multiplication table in java
    not working yet

---
+++ multiplicationtable.java
@@ -0,0 +1,14 @@
+// Multiplication table
+import java.util.Scanner;
+class multiplicationtable {
+    public static void main(String args[]) {
+        int n,c;
+        System.out.println("Enter an integer to print it's multiplication table");
+        Scanner in = new Scanner(System.in);
+        n= in.nextInt();
+        System.out.println("Multiplication table of "+n+" is :-");
+
+        for (c = 1; c<= 10; c++)
+            System.out.println(n+"*"+c+" = "+(n*c));
+    }
+}
