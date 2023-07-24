fccdd90052bb3070a88e920ea2dcb78bd3d64480
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-07-24 21:43:17 +0530

    java program to print multiplication table

---
+++ multi.java
@@ -0,0 +1,20 @@
+// multiplication table using java
+import java.util.Scanner;
+class multi {
+    public static void main(String args[]) {
+        int a, b, c, d;
+        System.out.println("Enter range of numbers to print their multiplication table");
+        Scanner in = new Scanner(System.in);
+
+        a = in.nextInt();
+        b = in.nextInt();
+
+        for (c =a; c <= b; c++) {
+            System.out.println("Multiplication table of "+c);
+
+            for (d=1; d<=10; d++) {
+                System.out.println(c+"*"+d+" = "+(c*d));
+            }
+        }
+    }
+}
