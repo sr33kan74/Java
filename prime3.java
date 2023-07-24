1572dcefb8c5ed4fbe426823dd89a13e90a9c0bc
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-07-24 22:00:00 +0530

    another java program to print prime number

---
+++ prime3.java
@@ -0,0 +1,31 @@
+// printing prime number using for loop
+import java.util.*;
+class prime3 {
+    public static void main(String args[]) {
+        int n, status = 1, num = 3;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter the number of prime numbers you want");
+        n = in.nextInt();
+
+        if (n >= 1); {
+            System.out.println("First "+n+" prime numbers are :-");
+            System.out.println(2);
+        }
+
+        for (int count = 2; count <=n ; ) {
+            for ( int j = 2; j<= Math.sqrt(num) ; j++) {
+                if (num%j ==0 ) {
+                    status = 0;
+                    break;
+                }
+            }
+            if (status != 0) {
+                System.out.println(num);
+                count++;
+            }
+            status = 1;
+            num++;
+        }
+    }
+}
