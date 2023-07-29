d69988718ac192b0ee2c1b1a7133532ec83df442
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-07-29 14:58:34 +0530

    Another program to rerverse a string

---
+++ reverse2.java
@@ -0,0 +1,7 @@
+//another program to print reverse of a string
+public class reverse2 {
+    public static void main(String args[]) {
+        StringBuffer a = new StringBuffer("Java programming is fun");
+        System.out.println(a.reverse());
+    }
+}
