783e47277136ffeb173d0a4a13caade6bd639e37
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-07-29 15:42:59 +0530

    Printing pattern

---
+++ triangle.java
@@ -0,0 +1,6 @@
+// program tp print tiangle with *
+public class triangle {
+    public static void main(String args[]) {
+        System.out.print("*\n**\n***\n****\n*****\n******\n*******\n");
+    }
+}
