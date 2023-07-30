df1cdeaa00b032b6f75492085d29158f5dadc555
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-07-30 16:22:49 +0530

    house keeping

---
+++ fun.java
@@ -0,0 +1,6 @@
+// program written for time pass
+public class fun {
+    public static void main(String[] args) {
+        System.out.print("          SSSSSSSSSS\n         S\n        S\n       SSSSSSSSSS\n               S\n              S\n    SSSSSSSSSS");
+    }
+}
