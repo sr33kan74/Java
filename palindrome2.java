1ab0c1e12aa8667ebe2905e889c0ff35b8b4d3a2
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-08-01 17:58:28 +0530

    palindrome

---
+++ palindrome2.java
@@ -0,0 +1,33 @@
+// program to check palindrome - method 2
+import java.util.*;
+class palindrome2  {
+    public static void main(String[] args) {
+        String inputString;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input a string");
+        inputString = in.nextLine();
+        int length = inputString.length();
+        int i, begin, end, middle;
+
+        begin = 0;
+        end = length - 1;
+        middle = (begin + end)/2;
+
+        for (i = begin; i <= middle; i++)  {
+            if (inputString.charAt(begin) == inputString.charAt(end)) {
+                begin++;
+                end--;
+            }
+            else {
+                break;
+            }
+        }
+        if (i == middle + 1) {
+            System.out.println("Palindrome");
+        }
+        else {
+            System.out.println("Not a palindrome");
+        }
+    }
+}
