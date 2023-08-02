328dea5a897586f7bc1ef1169392f52dd0a723d4
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-08-02 11:08:52 +0530

    Create scanner2.java

---
+++ scanner2.java
@@ -0,0 +1,12 @@
+// program to learn scanner
+import java.util.Scanner;
+public class scanner2 {
+    public static void main (String[] args) {
+        int a, int b;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please input a number to add");
+        a = in.nextLine();
+
+    }
+}
