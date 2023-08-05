efd15efb2dacf9825456b686a6853043dd245b31
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-08-05 23:27:37 +0530

    practice

---
+++ practice1.java
@@ -0,0 +1,15 @@
+// practicing some programs
+import java.util.Scanner;
+public class practice1 {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if(age > 18) {
+            System.out.println("Adult");
+        }
+        else {
+            System.out.println("Not Adult");
+        }
+    }
+}
