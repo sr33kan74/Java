7d2f4e149cb8fed442d79c8d1a63c1243c17a7d1
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-08-06 20:27:59 +0530

    practice

--- practice1.java
+++ practice1.java
@@ -56,4 +56,24 @@ public class practice1 {
 
  */
 
+
 // practicing if else in java
+/*
+import java.util.Scanner;
+public class practice1 {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = sc.nextInt();
+
+        if (age > 18) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("Minor")
+        }
+    }
+}
+
+ */
+
+// praticing if else in java
