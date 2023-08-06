93a2e19f9f7c1f99ba68470233e9ec06e9c607e6
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-08-06 21:56:43 +0530

    practice if else

--- practice1.java
+++ practice1.java
@@ -77,3 +77,16 @@ public class practice1 {
  */
 
 // praticing if else in java
+import java.util.Scanner;
+public class practice1 {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = sc.nextInt();
+        if(age > 18) {
+            System.out.println("You are an adult");
+        } else {
+            System.out.println("You are a minor please get out");
+        }
+    }
+}
