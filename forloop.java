9bfec9c3133f4d94a210c1e98ac44c7b50bce885
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-08-10 17:12:10 +0530

    practice

---
+++ forloop.java
@@ -0,0 +1,52 @@
+// practicing for loop
+/*
+public class forloop {
+    public static void main(String [] args) {
+        for (int i = 1; i <=10; i ++ ) {
+            System.out.println(i);
+        }
+    }
+}
+
+ */
+
+// practice 2
+
+/*
+public class forloop {
+    public static void main(String [] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+
+ */
+
+// for loop 3
+
+/*
+public class forloop {
+    public static void main(String [] args) {
+        for (int i = 1; i <= 20; i ++) {
+            System.out.print(i);
+        }
+    }
+}
+
+ */
+import java.util.Scanner;
+public class forloop {
+    public static void main (String [] args) {
+        Scanner sr = new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int n = sr.nextInt();
+        int factorial = 1;
+        for (int i = 1; i <= 10; i ++) {
+            factorial *= i;
+        }
+        System.out.println("Factorail of " + n + " is: " + factorial);
+    }
+}
