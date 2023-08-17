c9563f1834f82212b2995102fa56b05b33029c14
Sreekanth Rajeev <139335092+sr33kan74@users.noreply.github.com>
2023-08-17 20:06:01 +0530

    practice

--- forloop.java
+++ forloop.java
@@ -153,3 +153,20 @@ public class forloop {
 
  */
 // no comments
+
+// simple java program
+
+import java.util.Scanner;
+public class forloop {
+    public static void main (String [] args) {
+        Scanner sr = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int x = sr.nextInt();
+
+        if (x * 2 == 0)  {
+            System.out.println("The number is even");
+        } else {
+            System.out.println("The number is odd");
+        }
+    }
+}
